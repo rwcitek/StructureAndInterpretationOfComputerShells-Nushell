71d76fdfb7e6519bdfed2b505ad367174b9ed01d
Robert Citek <robert.citek@gmail.com>
2023-08-23 21:26:48 +0000

    Added jc and cleaned up install script

--- .devcontainer/install-nushell.sh
+++ .devcontainer/install-nushell.sh
@@ -2,8 +2,20 @@
 
 export DEBIAN_FRONTEND=noninteractive
 sudo apt-get update
-sudo apt-get install -y pkg-config libssl-dev jq tree vim curl less elinks sqlite3
+sudo apt-get install -y \
+       curl \
+       elinks \
+       jc \
+       jq \
+       less \
+       libssl-dev \
+       pkg-config \
+       sqlite3 \
+       tree \
+       vim \
+       ;
 
+# Fetch and install Nu shell
 cd /usr/local/bin/
 elinks --dump https://github.com/nushell/nushell/releases |
   grep -m1 -o https:.*x86_64-unknown-linux-gnu.tar.gz |
@@ -12,7 +24,7 @@ elinks --dump https://github.com/nushell/nushell/releases |
 sudo ln -snf ./nu-*-unknown-linux-gnu/nu
 yes | nu >& /dev/null || true
 
-
+# Create wrapper script so commands can be piped to Nu shell
 cat <<'eof' | sudo tee /usr/local/bin/nushell > /dev/null
 #!/usr/bin/env bash
 cat > /tmp/nushell.tmp.nu
